2f3e7263ed3eb5907efaac45e44d989fd901a423
Heguens NGOUGNI <heguens.ngougni@acesy.net>
2021-04-08 15:09:34 +0200

    creation of the model and utility file for the profile app

--- src/SBL/settings.py
+++ src/SBL/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'profiles',
+    'posts',
+    
+    # 'profiles.app.ProfilesConfig',
+    # 'posts.app.PostsConfig',
 ]
 
 MIDDLEWARE = [
