4cc9c3b6c6e9dd7658fd20ee6fb0b341ae95b2bb
Heguens NGOUGNI <heguens.ngougni@acesy.net>
2021-04-12 16:57:51 +0200

    added the base html template and semantic ui

--- src/SBL/views.py
+++ src/SBL/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def Home(request):
-    return HttpResponse(' home is working')
+    user = request.user
+    hello = 'hello'
+    context ={
+        'user': user,
+        'hello': hello
+    }
+    return render(request, 'main/index.html',context)
+    # return HttpResponse(' home is working')
